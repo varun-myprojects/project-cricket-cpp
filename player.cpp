d594b97646e8a50336fd581af0aac6b7c06cd17c
varun-myprojects <123awasthivarun321@gmail.com>
2020-07-26 17:01:01 +0530

    1st

---
+++ player.cpp
@@ -0,0 +1,12 @@
+#include "player.h"
+using namespace std;
+
+
+player::player(string s,int id)
+{
+    //cout<<uu;
+name=s;
+uid=id;
+runs_scored =   balls_bowled    =   balls_played    =   runs_given  =   wickets_taken =   0;
+
+}
